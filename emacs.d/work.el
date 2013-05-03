9cc6e6f8f8a588cc1e03897f84b036917056dbf4
Duncan Bayne <duncan.bayne@rea-group.com>
2013-05-03 15:52:25 +1000

    Removed stuff that should have been ignored.

--- emacs.d/work.el
+++ emacs.d/work.el
@@ -1,6 +1,1 @@
-(defun work:connect-to-irc()
-  "Connect to the REA IRC server"
-  (interactive)
-  (erc :server "ops01.int.realestate.com.au" :nick "dbayne" :full-name "Duncan Bayne"))
 
-(global-set-key (kbd "C-x e") 'work:connect-to-irc)
