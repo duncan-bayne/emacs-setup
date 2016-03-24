64c31ec57211220b2673627e5d560791bcffc581
Duncan Bayne <duncan@bayne.id.au>
2016-03-24 14:26:48 +1100

    UPGRADE ALL THE THINGS

---
+++ emacs.d/elpa/editorconfig-20160320.429/editorconfig-pkg.el
@@ -0,0 +1,1 @@
+(define-package "editorconfig" "20160320.429" "EditorConfig Emacs Plugin" '((editorconfig-core "0.6.2")) :url "https://github.com/editorconfig/editorconfig-emacs#readme")
--- emacs.d/elpa/request-20140316.417/request-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; request-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("request-pkg.el" "request.el") (22025
-;;;;;;  54938 351949 346000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; request-autoloads.el ends here
